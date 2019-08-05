ca851d5c0416d0526fd3405824e5daa993f274ec
singpaulee <hanifabdullah7@gmail.com>
2019-08-06 03:24:18 +0700

    Mengatur MainActivity.kt

--- app/src/main/java/com/singpaulee/submission_android_pemula_dicoding/MainActivity.kt
+++ app/src/main/java/com/singpaulee/submission_android_pemula_dicoding/MainActivity.kt
@@ -2,11 +2,30 @@ package com.singpaulee.submission_android_pemula_dicoding
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_cv_triassic.setOnClickListener(this)
+        main_cv_jurassic.setOnClickListener(this)
+        main_cv_cretaceous.setOnClickListener(this)
+        main_iv_about.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            main_cv_triassic -> startActivity(intentFor<CatalogDinosaurActivity>("periode" to "triassic"))
+            main_cv_jurassic -> startActivity(intentFor<CatalogDinosaurActivity>("periode" to "jurassic"))
+            main_cv_cretaceous -> toast("Maaf menu ini masih dalam pengembangan")
+            main_iv_about -> startActivity(intentFor<AboutAppActivity>())
+        }
     }
 }
