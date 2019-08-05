7e6be8d051b4b3a6fe48da2d7977a663e16622fe
singpaulee <hanifabdullah7@gmail.com>
2019-08-06 03:19:55 +0700

    Menambahkan About App Activity

---
+++ app/src/main/java/com/singpaulee/submission_android_pemula_dicoding/AboutAppActivity.kt
@@ -0,0 +1,12 @@
+package com.singpaulee.submission_android_pemula_dicoding
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_app)
+    }
+}
