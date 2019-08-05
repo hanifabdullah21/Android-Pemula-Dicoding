b00d93ad0d594837e25b9db8f66837eb86aef842
singpaulee <hanifabdullah7@gmail.com>
2019-08-06 03:21:00 +0700

    Menambah catalog dinosaurus activity

---
+++ app/src/main/java/com/singpaulee/submission_android_pemula_dicoding/CatalogDinosaurActivity.kt
@@ -0,0 +1,12 @@
+package com.singpaulee.submission_android_pemula_dicoding
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CatalogDinosaurActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalog_dinosaur)
+    }
+}
