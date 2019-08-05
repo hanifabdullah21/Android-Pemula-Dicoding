54302db5856256af705d5b160b43f294d641ae6f
singpaulee <hanifabdullah7@gmail.com>
2019-08-06 03:19:10 +0700

    Mengubah tampilan main activity

--- app/src/main/java/com/singpaulee/submission_android_pemula_dicoding/MainActivity.kt
+++ app/src/main/java/com/singpaulee/submission_android_pemula_dicoding/MainActivity.kt
@@ -1,34 +1,12 @@
 package com.singpaulee.submission_android_pemula_dicoding
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.GridLayoutManager
-import android.util.Log
-import com.singpaulee.submission_android_pemula_dicoding.adapter.DinosaurCatalogAdapter
-import com.singpaulee.submission_android_pemula_dicoding.helper.DinosaurBank.listDinosaurTriassic
-import com.singpaulee.submission_android_pemula_dicoding.model.DinosaurModel
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    val TAG = "MainActivity"
-
-    var layoutManager : GridLayoutManager? = null
-    var adapterDinosaur : DinosaurCatalogAdapter? = null
-    var listDinosaur : ArrayList<DinosaurModel>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        listDinosaur = listDinosaurTriassic
-
-        main_rv_dinosaur_catalog.hasFixedSize()
-        layoutManager = GridLayoutManager(this, 2)
-        adapterDinosaur = DinosaurCatalogAdapter(this, listDinosaur)
-        main_rv_dinosaur_catalog.layoutManager = layoutManager
-        main_rv_dinosaur_catalog.adapter = adapterDinosaur
-
-        Log.d(TAG, "Size of list : ${listDinosaur?.size}")
     }
 }
